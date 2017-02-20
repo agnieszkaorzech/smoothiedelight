2f1c6c3728522ff2e8d885a6b03c114009243560
agnieszkaorzech <agaorzech6@gmail.com>
2017-02-21 00:18:21 +0100

    webpack init

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 /**
  * Created by Agnieszka on 20.02.2017.
  */
+var OpenBrowserPlugin = require('open-browser-webpack-plugin');
 module.exports = {
     entry: './index.js',
     output: {
@@ -9,7 +10,13 @@ module.exports = {
     },
     module: {
         loaders: [
-            {test: /\.js$/, loader: 'babel-loader'}
+            {test: /\.js$/, loader: 'babel-loader'},
+            {test: /\.css$/, loader: 'style!css'}
         ]
-    }
+    },
+    plugins: [
+    new OpenBrowserPlugin({
+        url: 'http://localhost:8080'
+    })
+]
 };
